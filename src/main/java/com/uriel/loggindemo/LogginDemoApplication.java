c02ae4588718b078e9c398b52ed4b81cc9aa5e73
urielsb <uriel_sbarc@hotmail.com>
2018-11-06 15:26:38 -0800

    Init Version

---
+++ src/main/java/com/uriel/loggindemo/LogginDemoApplication.java
@@ -0,0 +1,12 @@
+package com.uriel.loggindemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogginDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogginDemoApplication.class, args);
+	}
+}
---
+++ src/main/java/com/uriel/loggindemo/web/LogginController.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.uriel.loggindemo.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Uriel Santoyo
+ *
+ */
+@RestController
+public class LogginController {
+
+	
+	private static final Logger logger = LoggerFactory.getLogger(LogginController.class);
+
+	@RequestMapping("/")
+	public String index() {
+		logger.trace("A TRACE Message");
+        logger.debug("A DEBUG Message");
+        logger.info("An INFO Message");
+        logger.warn("A WARN Message");
+        logger.error("An ERROR Message");
+		return "Que onda!";
+	}
+}
